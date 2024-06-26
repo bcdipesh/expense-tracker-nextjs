790e69214e5362449255fc5aed5ae90291dedfab
Dipesh B C <bcdipeshwork@gmail.com>
2024-06-25 22:53:12 -0400

    Add Balance component

---
+++ components/Balance.tsx
@@ -0,0 +1,13 @@
+import getUserBalance from "@/app/actions/getUserBalance";
+import { addCommas } from "@/lib/utils";
+
+export default async function Balance() {
+  const balance = await getUserBalance();
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${addCommas(balance.balance ?? 0)}</h1>
+    </>
+  );
+}
